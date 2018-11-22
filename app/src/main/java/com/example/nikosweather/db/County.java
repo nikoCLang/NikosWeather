d31fa9e477bcdf448a145f529740f0d47e76f283
nikoCLang <1768540781@qq.com>
2018-11-22 20:14:28 +0800

    First commit.

---
+++ app/src/main/java/com/example/nikosweather/db/County.java
@@ -0,0 +1,50 @@
+package com.example.nikosweather.db;
+
+/**
+ * Created by Nikos on 2018/11/19.
+ */
+
+public class County {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getCountyName(){
+        return countyName;
+    }
+
+    public void setCountyName(String countyName){
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId(){
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId(){
+        return cityId;
+    }
+
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+
+    public void save() {
+    }
+}
+
+
---
+++ app/src/main/java/com/example/nikosweather/db/Province.java
@@ -0,0 +1,47 @@
+package com.example.nikosweather.db;
+
+/**
+ * Created by Nikos on 2018/11/19.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void save() {
+
+    }
+
+    public static int size() {
+        return 0;
+    }
+}
+
+
+
